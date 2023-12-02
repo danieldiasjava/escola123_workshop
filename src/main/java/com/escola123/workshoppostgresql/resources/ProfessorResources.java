50af091f73284d1c5f3c44a5c451d78cee98343d
danieldiasjava <danieldias.devjava@gmail.com>
2023-12-01 22:00:35 -0300

    Implements Professor Resources and Service (findById, Update and Delete)

--- src/main/java/com/escola123/workshoppostgresql/resources/ProfessorResources.java
+++ src/main/java/com/escola123/workshoppostgresql/resources/ProfessorResources.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +30,27 @@ public class ProfessorResources {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Professor> findById(@PathVariable Long id) {
+		Professor obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	@PostMapping
 	public ResponseEntity<Professor> insert(@RequestBody Professor profObj){
 		profObj = service.insert(profObj);
 		return ResponseEntity.ok().body(profObj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Professor> update(@PathVariable Long id, @RequestBody Professor obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/escola123/workshoppostgresql/services/ProfessorService.java
+++ src/main/java/com/escola123/workshoppostgresql/services/ProfessorService.java
@@ -1,26 +1,51 @@
 package com.escola123.workshoppostgresql.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.escola123.workshoppostgresql.domain.Professor;
 import com.escola123.workshoppostgresql.repository.ProfessorRepository;
+import com.escola123.workshoppostgresql.services.exception.ObjectNotFoundException;
 
 @Service
 public class ProfessorService {
-	
+
 	@Autowired
 	private ProfessorRepository professorRepository;
-	
-	public List<Professor> findAll(){
+
+	public List<Professor> findAll() {
 		return professorRepository.findAll();
 	}
 
+	public Professor findById(Long id) {
+		Optional<Professor> obj = professorRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id nao encontrado"));
+
+	}
+
 	public Professor insert(Professor profObj) {
-	
+
 		return professorRepository.save(profObj);
+	}
+
+	public void delete(Long id) {
+		findById(id);
+		professorRepository.deleteById(id);
+	}
+
+	public Professor update(Long id, Professor obj) {
+		Professor entity = professorRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return professorRepository.save(entity);
+	}
+
+	private void updateData(Professor entity, Professor obj) {
+		entity.setName(obj.getName());
+		entity.setAddress(obj.getAddress());
+
 	}
 
 }
